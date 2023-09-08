efb02540aa5ce0e372d0dccf446ee774d9938c20
Jefferson Moraes <jeffersonjuniormoraes@gmail.com>
2023-09-08 00:59:40 -0300

    Implementando CRUD no Menu Principal

--- src/program/SistemaGerenciamentoEstudantes.java
+++ src/program/SistemaGerenciamentoEstudantes.java
@@ -3,6 +3,8 @@ package program;
 import java.util.Locale;
 import java.util.Scanner;
 
+import data.BancoDeDados;
+
 public class SistemaGerenciamentoEstudantes {
 
 	public static void main(String[] args) {
@@ -25,19 +27,52 @@ public class SistemaGerenciamentoEstudantes {
 			switch (opcaoMenu) {
 
 			case 1:
-				System.out.println("Voce escolheu ADICIONAR");
+				System.out.println("\n\t -- Voce escolheu ADICIONAR -- ");
+				BancoDeDados dbAdicionar = new BancoDeDados();
+				//INPUT USER
+				System.out.print("Insira o Nome: ");
+			    String nome = sc.next();
+			    sc.nextLine(); // Consuma a quebra de linha pendente
+			    System.out.print("Insira o Curso: ");
+			    String curso = sc.nextLine();
+				//instanciando - conexao com o banco				
+				dbAdicionar.adicionarEstudante(nome, curso);				
 				break;
 
 			case 2:
-				System.out.println("Voce escolheu EDITAR");
+				System.out.println("\n\t -- Voce escolheu EDITAR -- ");
+				BancoDeDados dbEditar = new BancoDeDados();
+				dbEditar.listarEstudante();
+				//INPUT
+				System.out.print("\nInforme o ID Estudante: ");
+				String idEditar = sc.next();
+				System.out.print("Digite o Nome Atualizado: ");
+				String nomeEditar = sc.next();
+				System.out.print("Digite o Curso Atualizado: ");
+				String cursoEditar = sc.next();
+				
+				//Insercao banco de dados
+				dbEditar.atualizarEstudante(idEditar, nomeEditar, cursoEditar);
 				break;
 
 			case 3:
-				System.out.println("Voce escolheu REMOVER");
+				System.out.println("\n\t -- Voce escolheu REMOVER -- ");
+				BancoDeDados dbDelete = new BancoDeDados();
+				dbDelete.listarEstudante(); //Será listado os estudantes com base na lista mostrada acima
+				//INPUT USER
+				System.out.print("\nInforme o Id: ");
+				String id = sc.next();
+				System.out.print("Você tem certeza que deseja excluir (y/n): ");
+				char opc = sc.next().charAt(0);
+				if(opc == 'y') {
+					dbDelete.removerEstudando(id);
+				}
 				break;
 
 			case 4:
 				System.out.println("Voce escolheu LISTAR");
+				BancoDeDados dbListar = new BancoDeDados();
+				dbListar.listarEstudante();		
 				break;
 
 			case 5:
